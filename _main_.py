d713457a828694d0e708a0bb1a44228e18a03c16
kingTwerk <102538612+kingTwerk@users.noreply.github.com>
2023-06-01 22:38:09 +0800

    Update _main_.py

--- _main_.py
+++ _main_.py
@@ -7,7 +7,8 @@ from streamlit_pandas_profiling import st_profile_report
 from pandas.errors import EmptyDataError
 from streamlit_lottie import st_lottie
 from streamlit_extras.colored_header import colored_header
-from functions import transformation_check, is_ordinal, load_lottiefile, load_lottieurl, get_sheet_names, normalize_numpy, filter_columns, transform_column, remove_file, download_csv
+from functions import transformation_check, is_ordinal, load_lottiefile, load_lottieurl, get_sheet_names, filter_columns, remove_file, download_csv
+from collections import defaultdict, Counter
 
 import openpyxl
 import os
@@ -31,7 +32,7 @@ import plotly.express as px
 import webbrowser
 import time
 
-st.set_page_config(page_title="INFER-X (051023)", layout='wide', initial_sidebar_state='expanded', page_icon="👁️‍🗨️")
+st.set_page_config(page_title="INFER-X (Local-updated 042523)", layout='wide', initial_sidebar_state='expanded', page_icon="👁️‍🗨️")
 
 
 lottie_hacking = load_lottiefile("lottiefiles/hacker.json")
@@ -70,10 +71,10 @@ def main():
     if show_sidebar: 
         if file is not None:
             file_extension = file.name.split(".")[-1]
-
+            na_values = ['N/A', 'NA', ' N/A', ' NA', 'N/A ', 'NA ', ' N/A ', ' NA ', '', '-', ' - ', ' -', '- ']
             if file_extension == "csv":
                 try:
-                    df = pd.read_csv(file, na_values=['N/A', 'NA',' N/A', ' NA','N/A ', 'NA ',' N/A ', ' NA ', '','-',' - ',' -', '- '])
+                    df = pd.read_csv(file, na_values=na_values)
                     df = df.apply(lambda x: x.str.strip() if isinstance(x[0], str) else x)           
                 except pd.errors.EmptyDataError:
                     st.sidebar.warning("WARNING: The file is empty. Please select a valid file.")
@@ -84,23 +85,13 @@ def main():
                 sheet_names = get_sheet_names(file)
                 if len(sheet_names) > 1:
                     selected_sheet = st.sidebar.selectbox("👉 SELECT AN EXCEL SHEET:", sheet_names)
-                    df = pd.read_excel(file, sheet_name=selected_sheet, na_values = ['N/A', 'NA',' N/A', ' NA','N/A ', 'NA ',' N/A ', ' NA ', '','-',' - ',' -', '- '])
+                    df = pd.read_excel(file, sheet_name=selected_sheet, na_values=na_values)
                 else:
-                    df = pd.read_excel(file, na_values= ['N/A', 'NA',' N/A', ' NA','N/A ', 'NA ',' N/A ', ' NA ', '','-',' - ',' -', '- '])
+                    df = pd.read_excel(file, na_values=na_values)
                 df = df.apply(lambda x: x.str.strip() if isinstance(x[0], str) else x)
                 if df.shape[0] == 0:
                     st.sidebar.warning("WARNING: The selected file or sheet is empty.")
             
-            # create a list of column options
-            # column_options = [f"{col}: {df[col].dtype}" for col in df.columns]
-
-            # loop over the column options and display information for each column
-            # for i, option in enumerate(column_options, start=1):
-            #     name, dtype = option.split(":")
-            #     num_blank = df[name.strip()].isna().sum()  # count the number of blank records
-            #     if num_blank > 0:  # display the information only if there is at least one blank record
-            #         st.write(f"{i}.&nbsp; <font color='blue'>{name.strip()} ({dtype.strip()}):</font> {num_blank} blank records", unsafe_allow_html=True)
-
             button, xl_drop_row, xl_drop_col = st.columns((0.0001,1.5,4.5), gap="small")
             
             xldrop_rows = df.shape[0]
@@ -112,36 +103,21 @@ def main():
                 st.markdown(f"<span style='color: blue;'>➕ Original # of columns : </span> <span style='color: black;'>{xldrop_cols}</span>", unsafe_allow_html=True)
 
             st.sidebar.title("👟 Infer-X Steps:") 
-            
-            for col in df.columns:
-                if df[col].dtype == 'object':
-                    df[col].fillna('-', inplace=True)
-                elif df[col].dtype == 'int64':
-                    df[col].fillna(0, inplace=True)
-                elif df[col].dtype == 'float64':
-                    df[col].fillna(0.0, inplace=True)
-                    
-                if df[col].dtype == 'datetime64[ns]':
-                    df[col] = df[col].dt.strftime('%Y-%m-%d')
-                    
+
+            object_cols = df.select_dtypes(include=['object']).columns
+            int_cols = df.select_dtypes(include=['int64']).columns
+            float_cols = df.select_dtypes(include=['float64']).columns
+            date_cols = df.select_dtypes(include=['datetime64[ns]']).columns
+
+            df[object_cols] = df[object_cols].fillna('-')
+            df[int_cols] = df[int_cols].fillna(0)
+            df[float_cols] = df[float_cols].fillna(0.0)
+
+            for col in date_cols:
+                df[col] = df[col].dt.strftime('%Y-%m-%d')
+  
             df_final = df
                 
-            # levels = np.empty(df_final.shape[1], dtype=object) 
-            # for i, col in enumerate(df_final.columns):  
-            #     if df_final[col].dtype == np.int64 or df_final[col].dtype == np.float64:
-            #         if df_final[col].dtype == np.int64:
-            #             levels[i] = "Discrete"
-            #         else:
-            #             levels[i] = "Continuous"
-            #     elif df_final[col].dtype == object:
-            #         if df_final[col].nunique() <= 2:
-            #             levels[i] = "Binary"
-            #         else:
-            #             if is_ordinal(df_final[col]):
-            #                 levels[i] = "Ordinal"
-            #             else:
-            #                 levels[i] = "Nominal"
-            
             levels = np.empty(df_final.shape[1], dtype=object) 
             for i, col in enumerate(df_final.columns):  
                 
@@ -164,58 +140,47 @@ def main():
                         if is_ordinal(df_final[col]):
                             levels[i] = "Ordinal"
                         else:
-                            levels[i] = "Nominal"
-            
-            
-            continuous_count, discrete_count, binary_count, nominal_count, ordinal_count = 0, 0, 0, 0, 0
+                             levels[i] = "Nominal"
 
-            for level in levels:
-                if level == "Continuous":
-                    continuous_count += 1
-                elif level == "Discrete":
-                    discrete_count += 1
-                elif level == "Binary":
-                    binary_count += 1
-                elif level == "Nominal":
-                    nominal_count += 1
-                elif level == "Ordinal":
-                    ordinal_count += 1
+                count_dict = defaultdict(int)
 
-            isNumerical = []
-            for columndata in df_final.columns:
-                if df_final[columndata].dtype == np.int64 or df_final[columndata].dtype == np.float64:
-                    isNumerical.append("Quantitative")
-                else:
-                    isNumerical.append("Categorical")
+                for level in levels:
+                    count_dict[level] += 1
 
-            recommendations = {}
-            for i, level in enumerate(levels):
-                if level == "Continuous":
-                    recommendations[df_final.columns[i]] = ["ANOVA", "SIMPLE LINEAR REGRESSION"]
-                elif level == "Discrete":
-                    recommendations[df_final.columns[i]] = ["ANOVA"]                    
-                elif level == "Binary":
-                    recommendations[df_final.columns[i]] = ["LOGISTIC REGRESSION"]
-                elif level == "Ordinal":
-                    recommendations[df_final.columns[i]] = ["CHI-SQUARE"]
-                elif level == "Nominal":
-                    recommendations[df_final.columns[i]] = ["CHI-SQUARE"]
-                else:
-                    recommendations[df_final.columns[i]] = ["NO RECOMMENDATION"]
+                continuous_count = count_dict["Continuous"]
+                discrete_count = count_dict["Discrete"]
+                binary_count = count_dict["Binary"]
+                nominal_count = count_dict["Nominal"]
+                ordinal_count = count_dict["Ordinal"]
 
-            anova_count, single_linear_regression_count, logistic_regression_count, chi_square_count = 0, 0, 0, 0
+            isNumerical = ["Quantitative" if pd.api.types.is_numeric_dtype(df_final[columndata]) else "Categorical" for columndata in df_final.columns]
+
+            level_to_recommendation = {
+                "Continuous": ["ANOVA", "SIMPLE LINEAR REGRESSION"],
+                "Discrete": ["ANOVA"],
+                "Binary": ["LOGISTIC REGRESSION", "CHI-SQUARE"],
+                "Ordinal": ["CHI-SQUARE"],
+                "Nominal": ["CHI-SQUARE"],
+            }
+
+            recommendations = {col: level_to_recommendation[level] for col, level in zip(df_final.columns, levels)}
+
+            test_to_count = {
+                "ANOVA": 0,
+                "SIMPLE LINEAR REGRESSION": 0,
+                "LOGISTIC REGRESSION": 0,
+                "CHI-SQUARE": 0,
+            }
 
             for tests in recommendations.values():
                 for test in tests:
-                    if test == "ANOVA":
-                        anova_count += 1
-                    elif test == "SIMPLE LINEAR REGRESSION":
-                        single_linear_regression_count += 1
-                    elif test == "LOGISTIC REGRESSION":
-                        logistic_regression_count += 1
-                    elif test == "CHI-SQUARE":
-                        chi_square_count += 1
-                               
+                    test_to_count[test] += 1
+
+            anova_count = test_to_count["ANOVA"]
+            single_linear_regression_count = test_to_count["SIMPLE LINEAR REGRESSION"]
+            logistic_regression_count = test_to_count["LOGISTIC REGRESSION"]
+            chi_square_count = test_to_count["CHI-SQUARE"]
+            
             column_numerical = [f"{columndata}: {isNumerical}" for columndata, isNumerical in zip(df_final.columns, isNumerical)]
             column_options = [f"{col}: {level}" for col, level in zip(df_final.columns, levels)]
             
@@ -225,15 +190,15 @@ def main():
                 color_name="violet-70",
             )   
             
-            # st.sidebar.write("1️⃣ Filter / Normalize / Transform Columns :")
             st.sidebar.write("1️⃣ FILTER BY COLUMN:")
             filter_checkbox = st.sidebar.checkbox("Filter (to exclude values)",key="checkbox3")
             
-            if filter_checkbox: # or transform_checkbox:
+            if filter_checkbox: 
                 if filter_checkbox:
                     df_final = filter_columns(df_final, filter_checkbox)
             else:
-                df_final = df.copy()    
+                df_final = df.copy()   
+                 
             column = st.sidebar.selectbox("2️⃣ SELECT THE 'y' FIELD (dependent variable):", list(recommendations.keys()))            
             test = st.sidebar.selectbox("3️⃣ SELECT AN INFERENTIAL TEST (determined by the selected 'y' field above):", recommendations[column])  
 
@@ -254,7 +219,6 @@ def main():
         
         st.sidebar.write("\n")                        
         details_checkbox = st.sidebar.checkbox("Do you want to more details?", key="checkbox0")
-        #profiling_checkbox = st.sidebar.checkbox("Raw file profiling?", key="checkbox10")
 
         if details_checkbox:
                 colored_header(
@@ -262,8 +226,7 @@ def main():
                     description="",
                     color_name="violet-70",
                     )   
-                # tab1, tab2, tab3, tab4, tab5, tab6, tab7  = st.tabs(["1️⃣ CURRENT COLUMN TEST SUGGESTIONS  |","2️⃣ LEVELS OF MEASUREMENTS COUNT  |","3️⃣ TEST SUGGESTIONS COUNT  |", "4️⃣ LEVELS OF MEASUREMENTS  |", "5️⃣ VARIABLE COLUMN TYPE  |","6️⃣ ALL COLUMNSS TEST SUGGESTIONS  |","7️⃣ NEEDS NORMALIZATION  |"])
-                # tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["𝟏┊NEEDS NORMALIZATION┊","𝟐┊MEASUREMENT TYPES┊","𝟑┊TEST SUGGESTIONS┊", "𝟒┊LEVELS OF MEASUREMENTS┊", "𝟓┊VARIABLE TYPES┊","𝟔┊ALL TEST SUGGESTIONS┊","Ⅰ. VARIABLE UNIQUE COUNT┊"])
+                st.subheader("[🔍] Variable Insights:")
                 tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["Ⅰ.┊NEEDS NORMALIZATION┊","Ⅱ.┊MEASUREMENT TYPES┊","Ⅲ.┊TEST SUGGESTIONS┊", "Ⅳ.┊LEVELS OF MEASUREMENTS┊", "Ⅴ.┊VARIABLE TYPES┊","Ⅵ.┊ALL TEST SUGGESTIONS┊","Ⅶ.┊UNIQUE VARIABLE┊","Ⅷ.┊PANDAS DTYPE┊"])
                 if not column:
                     st.write("No column selected")
@@ -276,8 +239,6 @@ def main():
                         name, dtype = option.split(":")
                         st.write(f"{i}.&nbsp; <font color='blue'>{name.strip()} ({dtype.strip()}):</font> {df_final[name.strip()].nunique()} unique values", unsafe_allow_html=True)
                 with tab2:
-                    # for i, (name, count) in enumerate(zip(["CONTINUOUS", "DISCRETE", "BINARY", "NOMINAL", "ORDINAL"], [continuous_count, discrete_count, binary_count, nominal_count, ordinal_count]), start=1):
-                    #     st.write(f"{i}.&nbsp; <font color='blue'>{name}:</font> {count}", unsafe_allow_html=True)  
                     for i, (name, dtype, count) in enumerate(zip(["CONTINUOUS", "DISCRETE", "BINARY", "NOMINAL", "ORDINAL"], ["float64", "int64", "int64", "object", "object"], [continuous_count, discrete_count, binary_count, nominal_count, ordinal_count]), start=1):
                         st.write(f"{i}.&nbsp; <font color='blue'>{name} ({dtype}):</font> {count}", unsafe_allow_html=True)                     
                 with tab3:
@@ -308,8 +269,6 @@ def main():
                     else:
                         st.write("No columns need to be normalized.")
                                 
-        # st.sidebar.divider()
-        # st.sidebar.title("🔍 RAW Data Overview:") 
         if st.sidebar.checkbox("Generate an extensive report providing insights into various aspects of your data.", key="pandas_profiling"): 
 
             option1=st.sidebar.radio(
@@ -368,7 +327,7 @@ def main():
                     description="",
                     color_name="violet-70",
                     )  
-                    st.subheader("[🔍] RAW Data Insights:")
+                    st.subheader("[🖨️] RAW Data Report:")
                     st_profile_report(profile) 
                     file_name = f"raw_data_overview_{file.name.split('.')[0]}.html"
                     profile.to_file(file_name)
